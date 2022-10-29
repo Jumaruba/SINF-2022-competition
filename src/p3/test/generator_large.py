11fd1301cfed07a2afb740890b2a2bdc03f03962
Juliane Marubayashi <juliane.marubayashi@gmail.com>
2022-10-29 17:28:42 +0100

    add: more tests for problem 3

---
+++ src/p3/test/generator_large.py
@@ -0,0 +1,6 @@
+# Generates the maximum number of elements as possible
+import random 
+for i in range(200000-1):
+    print(random.randint(0, 200), end= " ")
+
+print(random.randint(0,200))
---
+++ src/p3/test/generator_random.py
@@ -0,0 +1,6 @@
+# This is a random generator testing large numbers. 
+
+import random 
+for i in range(random.randint(1000, 200000-1)):
+    print(random.randint(0, 200), end=" ")
+print(random.randint(0,200))
